4c31fc93fcffdb4df5cdb1e289ab3c3adb570c92
tptiagopinto73 <2110906@my.ipleiria.pt>
2015-12-10 16:48:00 +0000

    hhhhh

--- WebServiceIS2015/IService1.cs
+++ WebServiceIS2015/IService1.cs
@@ -6,9 +6,9 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace WebServiceIS2015
+namespace WebServiceIS2015  
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Re  factor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
