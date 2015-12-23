92d01d83d36000c7fa1a31e8f1768b77e5910fd0
Nuno Domingues <2141239@my.ipleiria.pt>
2015-12-23 16:39:58 +0000

    sadasdasdsad

--- WebServiceIS2015/Service1.svc.cs
+++ WebServiceIS2015/Service1.svc.cs
@@ -5,13 +5,34 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Xml;
 
 namespace WebServiceIS2015
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Service1 : IService1
     {
+        XmlDocument xmlFile = new XmlDocument();
+        private Dictionary<string, User> users;
+        private Dictionary<string, Token> tokens;
+        private static string FILEPATH;
+
+
+
+        public Service1()
+        {
+            this.users = new Dictionary<string, User>();
+            this.tokens = new Dictionary<string, Token>();
+            // default administrator
+            users.Add("admin", new User("admin", "admin", true));
+            //FILEPATH = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "App_Data", "bookstore.xml");
+        }
+
+
+
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
